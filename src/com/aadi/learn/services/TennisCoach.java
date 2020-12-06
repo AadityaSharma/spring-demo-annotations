7188a3cdba445ffa4ee156543f22b1fd948e1f9a
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-06 17:37:41 +0530

    A basic demo for annotation based setter injection

--- src/com/aadi/learn/services/TennisCoach.java
+++ src/com/aadi/learn/services/TennisCoach.java
@@ -8,8 +8,21 @@ public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
 
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+    // define a default constructor
+
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
     @Autowired
-    public TennisCoach(FortuneService fortuneService) {
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside setFortuneService() method");
         this.fortuneService = fortuneService;
     }
 
