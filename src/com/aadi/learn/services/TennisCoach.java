b0f9e3bead162a9c4304c7170d1166045593d1fa
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-06 20:49:15 +0530

    A basic demo for understanding lifecycle of a bean. Use InitializingBean and DisposableBean interface methods

--- src/com/aadi/learn/services/TennisCoach.java
+++ src/com/aadi/learn/services/TennisCoach.java
@@ -1,5 +1,7 @@
 package com.aadi.learn.services;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 //@Scope("singleton")
-public class TennisCoach implements Coach{
+public class TennisCoach implements Coach, InitializingBean, DisposableBean {
 
     @Qualifier("randomFortuneService")
     @Autowired
@@ -51,4 +53,22 @@ public class TennisCoach implements Coach{
     public String getDailyFortune() {
         return fortuneService.getFortune();
     }
+
+    // this method will be executed after initialization of the bean
+    // @PostConstruct
+    // public void doMyStartupStuff() { }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(">> TennisCoach: inside of afterPropertiesSet() method");
+    }
+
+    // ths method will be executed after destroying the bean
+    // @PreDestroy
+    // public void doMyCleanupStuff() { }
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(">> TennisCoach: inside of destroy() method");
+    }
+
+
 }
